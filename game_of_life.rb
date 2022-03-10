5753e5f7b50754554655a24edad7acef26256cce
Anne Figer <anne-laure.figer@madetech.com>
2022-03-10 17:08:53 +0000

    trying to change each method to map but faffing around

--- game_of_life.rb
+++ game_of_life.rb
@@ -2,12 +2,12 @@ class GameOfLife
   def get_next_grid(current_grid)
     new_grid = current_grid
 
-    new_grid.each_with_index do |row, row_index|
-      row.each_with_index do |cell, column_index|
+    new_grid.map { |row, row_index|
+      row.map { |cell, column_index|
         living_neighbours_count = number_of_living_neighbours(row_index, column_index, new_grid)
         new_state_of_cell(cell, living_neighbours_count)
-      end
-    end
+      }
+    }
     
   end
 
